af91870ed345c554b0df3774596c219f378bc063
Bl1Zz <putin.dinu@gmail.com>
2019-03-10 20:08:00 +0200

    LinkedList 10.03.2019

---
+++ LinkedLists/cmake-build-debug/CMakeFiles/LinkedLists.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkedLists.dir/main.cpp.obj"
+  "CMakeFiles/LinkedLists.dir/source.cpp.obj"
+  "LinkedLists.pdb"
+  "LinkedLists.exe"
+  "LinkedLists.exe.manifest"
+  "libLinkedLists.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinkedLists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
